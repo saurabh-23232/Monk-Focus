33e69875430378921b4960020f04a5fe6bdd7921
Saurabh Kumar <76238822+saurabh-23232@users.noreply.github.com>
2025-07-24 12:37:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description:
     "Get Focus Just Like a Monk. Transform distractions into deep work with our complete productivity suite featuring Pomodoro timer, website blocker, and more.",
   icons: {
-    icon: "/public/favicon.ico.png",
+    icon: "/favicon.ico.png",
   },
 }
 
